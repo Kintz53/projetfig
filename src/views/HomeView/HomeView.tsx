e632394b88e6deadec6cbf37844779b294174e95
Kintz53 <kenny.bintz@outlook.fr>
2024-04-13 16:29:59 +0200

    be

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -24,31 +24,42 @@ type SectionProps = PropsWithChildren<{
 }>;
 
 function HomeView(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
+  
   const name = 'spartan with drake';
   const type = 'Titan';
-  const prix : number=90;
-  return (<View>
-    <Text>
-        This is a {type} miniature who cost {prix} euros. 
-    </Text>
-    <Text>
-        That the name of the miniature :{name}
-        <Image source={require('../../assets/Images/SpartanDrake.jpg')} style={styles.ImageFig}/>
-    </Text>
+  const prix : number=90;return (
+  <View>
+        <FigurineInfo name={name} type={type} prix={prix}/>,
   </View>
    
   );
  
+};
+
+const FigurineInfo =(props: any) => {
+<View>
+    <Text>
+        This is a {props.type} miniature who cost {props.prix} euros . 
+    </Text>
+    <View style={styles.container}>
+        <Text>
+        
+            That the name of the miniature :{props.name}
+            <Image source={require('../../assets/Images/SpartanDrake.jpg')} style={styles.ImageFig}/>
+        
+        </Text>
+    </View>
+</View>
+
 }
  const styles = StyleSheet.create({
     ImageFig :{
-        width: 600,
-        height: 200,
-    }
+        width: 200,
+        height: 500,
+    },
+    container : {
+        justifyContent: 'center',
+        alignItems: 'center',
+      }
   });
 export default HomeView;
